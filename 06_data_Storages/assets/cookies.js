3306ccff59c5887366c52647afdf8799e62169e9
unknown <rekrutenko.o@gmail.comm>
2021-08-01 21:09:25 +0300

    dataStorage update

--- 06_data_Storages/assets/cookies.js
+++ 06_data_Storages/assets/cookies.js
@@ -1,10 +1,7 @@
-console.log('Im cookies.js');
+console.log("i'm cookies.js")
 
-function setCokie (key, value, options) {
-    const expireData = new Date('07/31/2021').toUTCString();
-    document.cookie = `${key}=${value};
-    Expires=${expireData};
-    SameSite=Strict`;
+function setCookie (key, value, options) {
+    const expireDate = new Date('07/31/2021').toUTCString();
+    document.cookie = `${key}=${value};Expires=${expireDate};Max-Age=10;SameSite=Strict`;
 }
-
-setCokie('test', 'working');
+setCookie("test", "working");
--- 06_data_Storages/assets/webstorages.js
+++ 06_data_Storages/assets/webstorages.js
@@ -1,5 +1,14 @@
-if ('sesionStorage' in window) {
-
+if ('sessionStorage' in window) {
+    // when working with storages need to try/catch
+  try {
+    sessionStorage.setItem('name', 'Arturs');
+    const nameValueFromStorage = sessionStorage.getItem('name');
+    console.log(nameValueFromStorage);
+    sessionStorage.clear();
+    sessionStorage.removeItem('name');
+  } catch (err) {
+    console.error(err);
+  }
 } else {
-    console.log('Browser does not support sessionStorage');
+    console.log("Browser doesn't support session storage!");
 }
